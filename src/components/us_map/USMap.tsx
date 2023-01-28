a626d8c17ea2fd13840b756aeb726a9d2ae0c109
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2023-01-28 12:05:04 +0330

    move zoom in its own file

--- src/components/us_map/USMap.tsx
+++ src/components/us_map/USMap.tsx
@@ -1,5 +1,3 @@
-import * as d3 from "d3";
-import Config from "../../config/Config";
 import styles from './styles.module.css';
 import useMapData from "./hooks/useMapData";
 import useData from "../../context/hooks/useData";
@@ -9,18 +7,6 @@ import MapTitle from "./components/map_title/MapTitle";
 import States from "./components/state/States";
 import Counties from "./components/county/Counties";
 import useZoomControl from "./hooks/useZoomControl";
-
-const zoom = d3.zoom()
-.scaleExtent([1, 8])
-.on("zoom", (event) => {
-  const svg = d3.select(`#${Config.ElementIDs.Map}`);
-  const g = svg.select("g");
-  const {transform} = event;
-  g.attr("transform", transform);
-  g.attr("stroke-width", 1 / transform.k);
-});
-
-export { zoom };
 
 export default function USMap() {
   const mapData = useMapData();
