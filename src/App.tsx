51e31e13d1a5e58674dbf38aa074ac353f2d2696
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2023-02-01 20:12:59 +0330

    add hover data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Chart from './components/chart/Chart';
+import HoverData from './components/hover_data/HoverData';
 import USMap from './components/us_map/USMap';
 import ContextProvider from './context/ContextProvider';
 import styles from './styles.module.css';
@@ -6,6 +7,7 @@ import styles from './styles.module.css';
 function App() {
   return (
     <ContextProvider>
+      <HoverData />
       <div className={styles.main_container}>
         <USMap />
         <div className={styles.line} />
